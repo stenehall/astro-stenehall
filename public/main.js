50969b698af181b7c2e9375d2a5fa47107b85b2b
Johan Stenehall <stenehall@gmail.com>
2021-07-13 10:48:17 +0200

    Lazy load skills svgs

--- public/main.js
+++ public/main.js
@@ -1,3 +1,18 @@
 document.querySelector('#menu').addEventListener('click', evt => {
   document.querySelector('aside').classList.toggle('visible')
 })
+
+let options = {
+  rootMargin: '0px',
+  threshold: 0
+}
+
+const skills = document.querySelector('#skills')
+
+let observer = new IntersectionObserver((evt) => {
+  if(evt[0].isIntersecting) {
+    skills.classList.add('visible')
+  }
+}, options);
+
+observer.observe(skills)
